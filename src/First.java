ba56eda0b0d9c32ac31db6623c487119021ab432
Kanybek <m.kanybek@mail.ru>
2020-06-11 00:07:35 +0200

    added int

--- src/First.java
+++ src/First.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class First {
     public  static void main(String args[]){
         Scanner sc =  new Scanner(System.in);
+        int a = sc.nextInt();
         String st1 = sc.nextLine();
-        System.out.println(st1);
+        System.out.println(st1 + a);
+
     }
 }
