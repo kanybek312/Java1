1e19827980ae693a36dc9569ae9210af17326945
Kanybek <m.kanybek@mail.ru>
2020-06-11 00:03:35 +0200

    Create First.java

---
+++ src/First.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class First {
+    public  static void main(String args[]){
+        Scanner sc =  new Scanner(System.in);
+        String st1 = sc.nextLine();
+        System.out.println(st1);
+    }
+}
